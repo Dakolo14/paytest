54be8dedc3b83f74d86480b795b0006ded8ef4ca
Dakolo14 <ajosedare4u@gmail.com>
2024-07-07 09:21:16 +0100

    major commit

--- src/_landing/pages/Developer.tsx
+++ src/_landing/pages/Developer.tsx
@@ -1,10 +1,12 @@
 import DeveloperCard from "../components/DeveloperCard"
+import { Footer } from "../components/Footer"
 import { ScrollToTop } from "../components/ScrollToTop"
 
 const Developer = () => {
   return (
     <section>
       <DeveloperCard />
+      <Footer />
       <ScrollToTop />
     </section>
   )
